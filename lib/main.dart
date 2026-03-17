7b5050de8ef6c9f3c17b5ab3cd3eb03acb10cd4c
Sera_Dawgs <jaypeegazz@gmail.com>
2026-03-17 16:36:35 +0800

    Starter Template

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(home: Scaffold(body: XylophonePage())));
+}
+
+class XylophonePage extends StatefulWidget {
+  const XylophonePage({super.key});
+
+  @override
+  State<XylophonePage> createState() => _XylophonePageState();
+}
+
+class _XylophonePageState extends State<XylophonePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        //START HERE
+      ),
+    );
+  }
+}
